8a0b4612917bb5c347055f242dce5ce2cd135706
abdulmaleekadams <harbdulmaleek.com>
2023-08-12 09:49:18 +0100

    Set cors

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from routes import main_bp
 
 app = Flask(__name__)
 app.register_blueprint(main_bp)
+
+app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000', 'https://oc-predictor.vercel.app'])
 
 if __name__ == '__main__':
     app.run()
